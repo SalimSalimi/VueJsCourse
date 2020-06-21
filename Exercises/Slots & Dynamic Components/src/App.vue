cf667f284548eea6e878c361b0e26d3a48bdaafc
SalimSalimi <salim.salimi@protonmail.com>
2020-06-21 22:50:54 +0200

    Exercise Solution

--- Exercises/Slots & Dynamic Components/src/App.vue
+++ Exercises/Slots & Dynamic Components/src/App.vue
@@ -3,13 +3,24 @@
         <div class="row">
             <div class="col-xs-12">
                 <br>
-                <button class="btn btn-primary">Load Blue Template</button>
-                <button class="btn btn-success">Load Green Template</button>
-                <button class="btn btn-danger">Load Red Template</button>
+                <button class="btn btn-primary" @click="activatedComponent = 'appBlue'">Load Blue Template</button>
+                <button class="btn btn-success" @click="activatedComponent = 'appGreen'">Load Green Template</button>
+                <button class="btn btn-danger" @click="activatedComponent = 'appRed'">Load Red Template</button>
                 <hr>
-                <app-blue></app-blue>
-                <app-green></app-green>
-                <app-red></app-red>
+                <component :is="activatedComponent">
+                    {{ content }}
+                </component>
+                
+                <!-- First question
+                <app-blue>
+                    <p>{{ content }}</p>
+                </app-blue>        
+                <app-green>
+                    <p>{{ content }}</p>
+                </app-green>
+                <app-red>
+                    <p>{{ content }}</p>
+                </app-red> -->
             </div>
         </div>
     </div>
@@ -21,6 +32,12 @@
     import Red from './components/Red.vue';
 
     export default {
+        data : function() {
+            return {
+                content: "This is the content!",
+                activatedComponent: "appBlue"
+            }
+        },
         components: {
             appBlue: Blue,
             appGreen: Green,
