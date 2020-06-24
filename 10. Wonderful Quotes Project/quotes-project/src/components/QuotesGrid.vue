23095bf76ccdc28513e8c856b599b3ca5d029880
SalimSalimi <salim.salimi@protonmail.com>
2020-06-24 18:04:31 +0200

    Delete a Quote

--- 10. Wonderful Quotes Project/quotes-project/src/components/QuotesGrid.vue
+++ 10. Wonderful Quotes Project/quotes-project/src/components/QuotesGrid.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row">
-    <app-quote v-for="quote in quotes" :key="quote">
+    <app-quote v-for="(quote, index) in quotes" :key="quote" @click.native="deleteQuote(index)">
         {{ quote }}
     </app-quote>
   </div>
@@ -13,6 +13,11 @@
         props: ['quotes'],
         components: {
             appQuote: Quote
+        },
+        methods: {
+            deleteQuote(index) {
+                this.$emit('quoteDeleted', index);
+            }
         }
     }
 </script>
