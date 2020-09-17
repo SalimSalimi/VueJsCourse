ad61b6be87ff209603b4c01a6d8b6fd3b3e2c6fb
SalimSalimi <salim.salimi@protonmail.com>
2020-09-17 20:35:25 +0200

    Correction Exercise Custom Directives

--- Exercises/Directives/src/App.vue
+++ Exercises/Directives/src/App.vue
@@ -2,13 +2,44 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+      <h1>Custom Like v-on</h1>
+      <button v-customOn:click="onclicked" class="btn btn-rimary">Click Me!!</button>
+      <hr>
+      <div 
+        v-customOn:mouseover="mouseOver" v-customOn:mouseleave="mouseLeave"
+        style="width:100px; height:100px; background-color:lightgreen">
+      </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  directives: {
+    customOn: {
+      bind(el, binding) {
+        // el.onclick = () => {
+        //   binding.value();
+        // }
+        const type = binding.arg;
+        const func = binding.value;
+        el.addEventListener(type, func);
+      }
+    }
+  },
+  methods: {
+    onclicked(){
+      alert("Button Clicked");
+    },
+    mouseOver() {
+      console.log("Mouse over!");
+    },
+    mouseLeave() {
+      console.log("Mouse left!");
+    }
+  }
+};
 </script>
 
 <style></style>
