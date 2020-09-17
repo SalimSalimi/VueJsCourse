375412febfed8eaf7b394f2f3b99d8a73c2e3274
SalimSalimi <salim.salimi@protonmail.com>
2020-09-17 15:49:36 +0200

    12. Using and creating directives

---
+++ 12. Using and Creating Directives/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <form>
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
+        ></div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
