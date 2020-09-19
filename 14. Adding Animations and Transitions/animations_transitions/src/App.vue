e39f4dc34af253b3982dd0690f1c7aeca303af4a
SalimSalimi <salim.salimi@protonmail.com>
2020-09-19 11:46:02 +0200

    14. Animations & Transitions Initial commit

---
+++ 14. Adding Animations and Transitions/animations_transitions/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <div
+      class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+      <h1>Animations & Transitions</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
