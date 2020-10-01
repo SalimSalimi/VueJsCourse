32bad943cceb66a245413f7631ddc6263f4b89ac
SalimSalimi <salim.salimi@protonmail.com>
2020-10-01 18:46:09 +0200

    Handling no data state

--- 15. Sending Http Requests/src/components/survey/UserExperiences.vue
+++ 15. Sending Http Requests/src/components/survey/UserExperiences.vue
@@ -6,7 +6,8 @@
         <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
       <p v-if="isLoading">Loading...</p>
-      <ul v-else>
+      <p v-else-if="!isLoading && (!results || results.length === 0)">No stored experiences found. Start to create new ones.</p>
+      <ul v-else-if="!isLoading && results && results.length > 0">
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -39,6 +40,7 @@ export default {
           return response.json();
         }
       }).then(data => {
+        this.isLoading = false;
         const results = [];
         for (const id in data) {
           results.push({
@@ -47,7 +49,7 @@ export default {
             rating: data[id].rating
           });
           this.results = results;
-          this.isLoading = false;
+
         }
       });
     }
